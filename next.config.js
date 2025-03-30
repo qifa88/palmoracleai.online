f90a7e866c6f47dea4a6c6f54fbdcb65073624f5
qifa88 <chafa9168@gmail.com>
2025-03-30 20:48:08 +0800

    添加静态HTML解决404问题

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 module.exports = {
   images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      { protocol: 'http', hostname: '**' },
-      { protocol: 'https', hostname: '**' }
-    ],
     unoptimized: true
   },
   output: 'export',
   trailingSlash: true,
-  assetPrefix: './',
+  distDir: 'out',
   exportPathMap: async function() {
     return {
-      '/': { page: '/' },
-      '/analyze': { page: '/analyze' },
-      '/result': { page: '/result' },
-      '/404': { page: '/404' },
+      '/': { page: '/' }
     };
   }
 } 
